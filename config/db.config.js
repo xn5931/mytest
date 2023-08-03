63e4fc88834b0fc3ac3f7590755077434ce75cbe
xn5931 <hailian159@gmail.com>
2023-08-03 22:36:14 +0800

    d

--- config/db.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "mmmfi7799@fi",
-  DB: "community"
-};
--- routes/tutorial.routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-  const tutorials = require("../controllers/tutorial.controller.js");
-
-  var router = require("express").Router();
-  router.post("/register",tutorials.register);
-  router.post("/userInfo",tutorials.findOne);
-  app.use('/', router);
-};
